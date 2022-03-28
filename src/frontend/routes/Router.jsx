a0ac9d9c746601900b3365f206e4c5416bfe6194
Viresh <vireshfegade@gmail.com>
2022-03-28 14:53:17 +0530

    feat : add private route for cart and wishlist

--- src/frontend/routes/Router.jsx
+++ src/frontend/routes/Router.jsx
@@ -2,17 +2,20 @@ import { Routes, Route } from "react-router-dom";
 import Mockman from "mockman-js";
 
 import { Home, Products, Cart, Wishlist, Signin, Signup } from "../pages";
+import { RequireAuth } from "../components";
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/products" element={<Products />} />
-      <Route path="/cart" element={<Cart />} />
-      <Route path="/wishlist" element={<Wishlist />} />
       <Route path="/signin" element={<Signin />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/mock" element={<Mockman />} />
+      <Route path="/" element={<RequireAuth />}>
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/wishlist" element={<Wishlist />} />
+      </Route>
     </Routes>
   );
 };
