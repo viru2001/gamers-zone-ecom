6334bb4587e00c9a1b5803e3df41792cccca37f0
Viresh <vireshfegade@gmail.com>
2022-04-28 23:50:49 +0530

    Update Footer.jsx

--- src/frontend/components/Footer/Footer.jsx
+++ src/frontend/components/Footer/Footer.jsx
@@ -5,38 +5,6 @@ const Footer = () => {
       <div className="text-sm font-wt-semibold">
         Made with ❤️ by Viresh Fegade, Ajay Chodankar, Abhishek Bhingle
        </div>
-//       <ul className="d-flex justify-center align-center">
-//         <li className="m-2">
-//           <a
-//             className="text-md"
-//             target="_blank"
-//             href="https://github.com/viru2001"
-//             rel="noreferrer"
-//           >
-//             <i className="footer-social-icon fab fa-github"></i>
-//           </a>
-//         </li>
-//         <li className="m-2">
-//           <a
-//             className="text-md"
-//             target="_blank"
-//             href="https://twitter.com/fegade_viresh"
-//             rel="noreferrer"
-//           >
-//             <i className="footer-social-icon fab fa-twitter"></i>
-//           </a>
-//         </li>
-//         <li className="m-2">
-//           <a
-//             className="text-md"
-//             target="_blank"
-//             href="https://www.linkedin.com/in/viresh-fegade-43a475148/"
-//             rel="noreferrer"
-//           >
-//             <i className="footer-social-icon fab fa-linkedin-in"></i>
-//           </a>
-//         </li>
-//       </ul>
     </footer>
   );
 };
