6c2858914d02eea3c27f1f702dcdf9e8710c92e1
Viresh <vireshfegade@gmail.com>
2022-04-28 23:47:53 +0530

    Update Footer.jsx

--- src/frontend/components/Footer/Footer.jsx
+++ src/frontend/components/Footer/Footer.jsx
@@ -3,40 +3,40 @@ const Footer = () => {
   return (
     <footer className="footer text-center p-4 mt-4">
       <div className="text-sm font-wt-semibold">
-        Made with ❤️ by Viresh Fegade
-      </div>
-      <ul className="d-flex justify-center align-center">
-        <li className="m-2">
-          <a
-            className="text-md"
-            target="_blank"
-            href="https://github.com/viru2001"
-            rel="noreferrer"
-          >
-            <i className="footer-social-icon fab fa-github"></i>
-          </a>
-        </li>
-        <li className="m-2">
-          <a
-            className="text-md"
-            target="_blank"
-            href="https://twitter.com/fegade_viresh"
-            rel="noreferrer"
-          >
-            <i className="footer-social-icon fab fa-twitter"></i>
-          </a>
-        </li>
-        <li className="m-2">
-          <a
-            className="text-md"
-            target="_blank"
-            href="https://www.linkedin.com/in/viresh-fegade-43a475148/"
-            rel="noreferrer"
-          >
-            <i className="footer-social-icon fab fa-linkedin-in"></i>
-          </a>
-        </li>
-      </ul>
+        Made with ❤️ by Viresh Fegade, Ajay Chodankar, Abhishek Bhingle
+       </div>
+//       <ul className="d-flex justify-center align-center">
+//         <li className="m-2">
+//           <a
+//             className="text-md"
+//             target="_blank"
+//             href="https://github.com/viru2001"
+//             rel="noreferrer"
+//           >
+//             <i className="footer-social-icon fab fa-github"></i>
+//           </a>
+//         </li>
+//         <li className="m-2">
+//           <a
+//             className="text-md"
+//             target="_blank"
+//             href="https://twitter.com/fegade_viresh"
+//             rel="noreferrer"
+//           >
+//             <i className="footer-social-icon fab fa-twitter"></i>
+//           </a>
+//         </li>
+//         <li className="m-2">
+//           <a
+//             className="text-md"
+//             target="_blank"
+//             href="https://www.linkedin.com/in/viresh-fegade-43a475148/"
+//             rel="noreferrer"
+//           >
+//             <i className="footer-social-icon fab fa-linkedin-in"></i>
+//           </a>
+//         </li>
+//       </ul>
     </footer>
   );
 };
